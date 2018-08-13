a005e9d7bb5da069e5d7a1070b1fd858f90b225c
aboud20-meet <aboud20@meet.mit.edu>
2018-08-13 11:20:37 +0300

    efefr

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
 @app.route('/add_event',  methods=['GET', 'POST'])
 def add_event_route():
     if(request.method == 'GET'):
-        return render_template("add.html")
+        return render_template("add_event.html")
     else:                                                          
         ev_name = request.form['event_name']
         ev_loction = request.form['event_location']
@@ -32,8 +32,8 @@ def add_user_route():
         us_name = request.form['user_name']
         us_loction = request.form['user_location']
         us_age = request.form['user_age']
-        add_user(us_name,us_age,us_location)
-        login_session['id']=user.id
+        user = add_user(us_name,us_age,us_location)
+        login_session['id']=user.user_name
     
         return render_template("home.html")
 
